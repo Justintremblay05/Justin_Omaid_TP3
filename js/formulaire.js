e5be7d85c777d5470ad74ae9cdcb3ae75341a565
Justintremblay05 <166869957+Justintremblay05@users.noreply.github.com>
2025-04-08 08:57:51 -0400

    [feat] ajout des validation des champs

--- js/formulaire.js
+++ js/formulaire.js
@@ -3,7 +3,54 @@
 
 /* DÉBUT variables globales */ 
 // ##########################
+let prenom = document.getElementById("prenom");
+let nom = document.getElementById("nom");
+let courriel = document.getElementById("courriel");
+let confirmation = document.getElementById("confirmation");
+let pseudo = document.getElementById("pseudo");
 
+
+
+
+/* FIN variables globales */ 
+// ##########################
+
+function validerCourriel(courriel) {
+	let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	let msgErreur = document.getElementById("msgErreurMail");
+	msgErreur.textContent = "";
+	if (regex.test(courriel)) {
+		return true;
+	}
+	else{
+        msgErreur.textContent = "Le courriel n'est pas valide.";
+		return false;
+	}
+}
+function validerConfirmation(confirmation) {
+	let msgErreur = document.getElementById("msgErreurConfirmation");
+	msgErreur.textContent = "";
+	if (confirmation.value === courriel.value) {
+		return true;
+	}
+	else{
+		msgErreur.textContent = "Les courriels ne correspondent pas.";
+		return false;
+	}
+}
+
+function validerPseudo(pseudo) {
+	let regex = /^[a-zA-Z]{3,25}$/;
+	let msgErreur = document.getElementById("msgErreurPseudo");
+	msgErreur.textContent = "";
+	if (regex.test(pseudo)) {
+		return true;
+	}
+	else{
+		msgErreur.textContent = "Le pseudo doit contenir entre 3 et 25 caractères.";
+		return false;
+	}
+}
 
 function afficherChoixJeu()
 {
